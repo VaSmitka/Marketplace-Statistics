01786b53729ebc778aeb2865d0d995193235222f
Václav Smítka <va.smitka@gmail.com>
2021-05-03 21:06:21 +0200

    jest better end 4

--- __tests__/basicIntegrationTest.js
+++ __tests__/basicIntegrationTest.js
@@ -1,11 +1,12 @@
 const db = require("../models");
 
+beforeAll(() => {
+  // Clears the database and adds some testing data.
+  // Jest will wait for this promise to resolve before running tests.
+  return db.sequelize.sync({ force: true }).then(() => console.log("Drop and re-sync db."));
+});
+
 describe('integration test jest', () => {
-  beforeAll(() => {
-    // Clears the database and adds some testing data.
-    // Jest will wait for this promise to resolve before running tests.
-    return db.sequelize.sync({ force: true }).then(() => console.log("Drop and re-sync db."));
-  });
 
   test('is delicious', () => {
     return db['User'].findAll().then(users => 
@@ -13,5 +14,7 @@ describe('integration test jest', () => {
     );
   });
 
-  // afterAll(() => process.exit(0));
+  afterAll(() => {
+    return db.sequelize.connectionManager.close().then(() => console.log('shut down gracefully'));
+  });
 });
